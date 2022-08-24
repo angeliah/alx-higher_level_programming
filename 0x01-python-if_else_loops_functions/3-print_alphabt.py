87bec31c59b43054600b3e1d98e4426b5161c9f4
angeliah <angeliaowango@gmail.com>
2022-08-24 12:12:27 -0700

    Fixing pycodestyle for 3-print_alphbt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,6 +2,6 @@
 # 3-print_alphabt.py
 
 """ Print alphabet letters except q and e"""
-for i in range (97, 123):
+for i in range(97, 123):
     if chr(i) is not 'e' and chr(i) is not 'q':
         print("{}".format(chr(i)), end="")
