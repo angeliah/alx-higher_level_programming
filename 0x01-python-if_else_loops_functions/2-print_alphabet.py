ee0e9b9756047c137cb945cca1619602cadfeab8
angeliah <angeliaowango@gmail.com>
2022-08-24 06:08:36 -0700

    fixing file 2 errors

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#2-print_alphabet.py
+# 2-print_alphabet.py
 
 """Print alphabet in lowercase,not followed by a new line"""
-for i in range(97,123):
-    print("{}".format(chr(i)),end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
