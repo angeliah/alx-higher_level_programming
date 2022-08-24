53472707d8a18afc25b274d8cde06efe5ed22742
angeliah <angeliaowango@gmail.com>
2022-08-24 04:03:24 -0700

    fixing file 0

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,6 +5,5 @@ if((number > 0)):
     print(number, "is positive")
 elif number == 0:
     print(number, "is zero")
-elif number > 0:
-    print(number, "is negative")
-
+elif number < 0:
+    print(number, "is negative\n")
