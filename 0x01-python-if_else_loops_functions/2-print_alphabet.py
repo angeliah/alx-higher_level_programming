915ec02a5b737e39421b40007908e62085f94225
angeliah <angeliaowango@gmail.com>
2022-08-24 06:05:54 -0700

    Fix of file 2

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print(f"abcdefghijklmnopqrstuvwxyz",end="")
+#2-print_alphabet.py
+
+"""Print alphabet in lowercase,not followed by a new line"""
+for i in range(97,123):
+    print("{}".format(chr(i)),end="")
