d965f621aaa59b6222bebb58b3857f0e7e002427
angeliah <angeliaowango@gmail.com>
2022-09-13 01:13:52 -0700

    Fix of file 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,8 +2,6 @@
 
 # 5-print_comb2
 """prints numbers from 0 to 99,separated by coma,in two digits"""
-for i in range(0, 100):
-    if i == 99:
-        print("{}".format(i))
-    else:
-        print("{:02}".format(i), end=", ")
+for i in range(0, 99):
+    print("{:02d}, ".format(i), end='')
+print("99");
---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# 6-print_comb3
+"""prints all possible different combinations of two digits"""
+for i in range(1, 100):
+    for j in range(1 ,100):
+        if i != k and i == 99:
+            print("{}".format(i)
+        else:
+            print("{:02}".format(i), end=", "))
