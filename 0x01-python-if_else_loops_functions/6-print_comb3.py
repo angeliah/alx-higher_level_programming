9884ef8b076bd1a385d56a9b704098809c46d476
angeliah <angeliaowango@gmail.com>
2022-09-13 01:36:43 -0700

    Fix of file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,9 +2,7 @@
 
 # 6-print_comb3
 """prints all possible different combinations of two digits"""
-for i in range(1, 100):
-    for j in range(1 ,100):
-        if i != k and i == 99:
-            print("{}".format(i)
-        else:
-            print("{:02}".format(i), end=", "))
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
